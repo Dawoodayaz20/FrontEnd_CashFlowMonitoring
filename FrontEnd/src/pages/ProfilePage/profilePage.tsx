d8a4ac54dab0aa3da5d6a05f0ae2b3a86b1d960e
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-03-20 22:19:04 +0500

    Change hidepass emoji with svg eye icon.

--- FrontEnd/src/pages/ProfilePage/profilePage.tsx
+++ FrontEnd/src/pages/ProfilePage/profilePage.tsx
@@ -309,7 +309,41 @@ const ProfilePage: React.FC = () => {
                   onClick={() => toggleShowPassword("current")}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 text-sm"
                 >
-                  {showPasswords["current"] ? "🙈" : "👁️"}
+                  {showPasswords["current"] 
+                  ? 
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                      width="20" height="20" 
+                      fill="none" 
+                      viewBox="0 0 24 24" 
+                      stroke="currentColor" 
+                      strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5
+                        c4.477 0 8.268 2.943 9.542 7
+                        -1.274 4.057-5.065 7-9.542 7
+                        -4.477 0-8.268-2.943-9.542-7z" />
+                    </svg> 
+                  :
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        width="20" height="20" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        strokeWidth="2">
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M13.875 18.825A10.05 10.05 0 0112 19
+                          c-4.477 0-8.268-2.943-9.542-7
+                          a9.956 9.956 0 012.042-3.368M6.7 6.7
+                          A9.956 9.956 0 0112 5
+                          c4.477 0 8.268 2.943 9.542 7
+                          a9.97 9.97 0 01-4.043 4.568M15 12
+                          a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M3 3l18 18" />
+                    </svg>
+              }
                 </button>
               </div>
               {pwErrors.current && <p className="mt-1 text-xs text-rose-500">{pwErrors.current}</p>}
@@ -335,7 +369,41 @@ const ProfilePage: React.FC = () => {
                   onClick={() => toggleShowPassword("next")}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 text-sm"
                 >
-                  {showPasswords["next"] ? "🙈" : "👁️"}
+                  {
+                  showPasswords["next"] ? 
+                  <svg xmlns="http://www.w3.org/2000/svg" 
+                width="20" height="20" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke="currentColor" 
+                strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5
+                  c4.477 0 8.268 2.943 9.542 7
+                  -1.274 4.057-5.065 7-9.542 7
+                  -4.477 0-8.268-2.943-9.542-7z" />
+              </svg> 
+            :
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                  width="20" height="20" 
+                  fill="none" 
+                  viewBox="0 0 24 24" 
+                  stroke="currentColor" 
+                  strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19
+                    c-4.477 0-8.268-2.943-9.542-7
+                    a9.956 9.956 0 012.042-3.368M6.7 6.7
+                    A9.956 9.956 0 0112 5
+                    c4.477 0 8.268 2.943 9.542 7
+                    a9.97 9.97 0 01-4.043 4.568M15 12
+                    a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M3 3l18 18" />
+              </svg>
+                  }
                 </button>
               </div>
               {pwErrors.next && <p className="mt-1 text-xs text-rose-500">{pwErrors.next}</p>}
@@ -378,7 +446,42 @@ const ProfilePage: React.FC = () => {
                   onClick={() => toggleShowPassword("confirm")}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 text-sm"
                 >
-                  {showPasswords["confirm"] ? "🙈" : "👁️"}
+                  {
+                  showPasswords["confirm"]
+                   ? 
+                   <svg xmlns="http://www.w3.org/2000/svg" 
+                      width="20" height="20" 
+                      fill="none" 
+                      viewBox="0 0 24 24" 
+                      stroke="currentColor" 
+                      strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5
+                        c4.477 0 8.268 2.943 9.542 7
+                        -1.274 4.057-5.065 7-9.542 7
+                        -4.477 0-8.268-2.943-9.542-7z" />
+                    </svg> 
+                  :
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        width="20" height="20" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        strokeWidth="2">
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M13.875 18.825A10.05 10.05 0 0112 19
+                          c-4.477 0-8.268-2.943-9.542-7
+                          a9.956 9.956 0 012.042-3.368M6.7 6.7
+                          A9.956 9.956 0 0112 5
+                          c4.477 0 8.268 2.943 9.542 7
+                          a9.97 9.97 0 01-4.043 4.568M15 12
+                          a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M3 3l18 18" />
+                    </svg>
+                  }
                 </button>
               </div>
               {pwErrors.confirm && <p className="mt-1 text-xs text-rose-500">{pwErrors.confirm}</p>}
