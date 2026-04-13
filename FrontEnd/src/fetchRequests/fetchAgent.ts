0241279eaf72a9b7c3158baf26b8ee7a98d5acf9
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-04-13 23:17:40 +0500

    create flowagent api for sending request.

---
+++ FrontEnd/src/fetchRequests/fetchAgent.ts
@@ -0,0 +1,27 @@
+// type PersonalInfo = {
+//     id: string,
+//     name: string,
+//     email: string,
+// }
+
+export const getResponse = async (text: string, userID: string) => {
+
+    try{
+        const response = await fetch(`${import.meta.env.VITE_FLOWAGENT_API}`, {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({ question: text, userID })
+    })
+
+    if (!response.ok){
+        alert("There was an error connecting to Server!");
+    }
+
+    const data = await response.json();
+    console.log("Response:", data);
+    return data;
+    }
+    catch(err){
+        console.log('There was an error connecting to Server:', err);
+    }
+}
