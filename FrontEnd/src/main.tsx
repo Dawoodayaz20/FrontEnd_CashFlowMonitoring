df0b13d4740d4e2cf619517cf1e0695d76849a72
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-04-23 16:23:13 +0500

    Wrap the app in chatsContextProvider.

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ChatContextProvider } from './pages/FlowManagerPage/ChatContext.tsx';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
+    <ChatContextProvider>
       <App />
+    </ChatContextProvider>
     </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
