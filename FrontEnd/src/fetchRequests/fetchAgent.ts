1b19fa65365824205786f1a91f4c6608aee1fe4e
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-05-02 03:55:36 +0500

    send session_id to agent for chat_context.

--- FrontEnd/src/fetchRequests/fetchAgent.ts
+++ FrontEnd/src/fetchRequests/fetchAgent.ts
@@ -1,10 +1,10 @@
-export const getResponse = async (text: string, userID: string, userName: string, email: string) => {
+export const getResponse = async (text: string, userID: string, userName: string, email: string, activeSessionId: string) => {
 
     try{
         const response = await fetch(`${import.meta.env.VITE_FLOWAGENT_API}`, {
         method: "POST",
         headers: {'Content-type': 'application/json'},
-        body: JSON.stringify({ question: text, userID, user_name: userName, email })
+        body: JSON.stringify({ question: text, userID, user_name: userName, email, session_id: activeSessionId })
     })
 
     const data = await response.json();
@@ -57,3 +57,5 @@ export const generateTitle = async function generateChatTitle(userFirstMessage: 
 // Example usage:
 // const title = await generateChatTitle("How do I cook a perfect steak?");
 // console.log(title); // "Perfect Steak Cooking"
+
+// There was an error connecting the Server:Error code: 429 - [{'error': {'code': 429, 'message': 'You exceeded your current quota, please check your plan and billing details. For more information on this error, head to: https://ai.google.dev/gemini-api/docs/rate-limits. To monitor your current usage, head to: https://ai.dev/rate-limit. \n* Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_requests, limit: 20, model: gemini-2.5-flash\nPlease retry in 51.946480516s.', 'status': 'RESOURCE_EXHAUSTED', 'details': [{'@type': 'type.googleapis.com/google.rpc.Help', 'links': [{'description': 'Learn more about Gemini API quotas', 'url': 'https://ai.google.dev/gemini-api/docs/rate-limits'}]}, {'@type': 'type.googleapis.com/google.rpc.QuotaFailure', 'violations': [{'quotaMetric': 'generativelanguage.googleapis.com/generate_content_free_tier_requests', 'quotaId': 'GenerateRequestsPerDayPerProjectPerModel-FreeTier', 'quotaDimensions': {'model': 'gemini-2.5-flash', 'location': 'global'}, 'quotaValue': '20'}]}, {'@type': 'type.googleapis.com/google.rpc.RetryInfo', 'retryDelay': '51s'}]}}]
